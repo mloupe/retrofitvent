0ba0f3cec30e0a5d179d97d99b4cce7be68f68cd
Matthew Loupe <mloupe2@gmail.com>
2020-03-30 17:02:42 -0500

    script version used in video

--- scripts/vent.sh
+++ scripts/vent.sh
@@ -1,21 +1,21 @@
+#define device
+dev="/dev/cu.usbmodem621"
+
 #initialize connection and set baud rate
-exec 3<> /dev/cu.usbmodem621
-stty -F /dev/cu.usbmodem621 115200
-stty -f /dev/cu.usbmodem621 115200
-stty -f /dev/cu.usbmodem621 speed
+exec 3<> $dev
+stty -F $dev 115200
+stty -f $dev 115200
+stty -f $dev speed
 
 
-#echo G1 Z-190.00 F500 > /dev/cu.usbmodem621
-
-
-while read -r line < /dev/cu.usbmodem621; do
+while read -r line < $dev; do
   # $line is the line read, do something with it
   echo $statuscode
   statuscode=$line
 
         if [ "$statuscode" == "ok" ]; then
-                echo G1 Y100.00 F500 > /dev/cu.usbmodem621
-                echo G1 Y150.00 F500 > /dev/cu.usbmodem621
+		echo G1 Z92.00 F300 > $dev
+                echo G1 Z80.00 F300 > $dev
         elif [ "$statuscode" == "no" ]; then
                 break
         fi
